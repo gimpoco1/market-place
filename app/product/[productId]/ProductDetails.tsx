3b99db1bb94b2a60d587e52b802d6235da83973b
gimpoco1 <impoco126@gmail.com>
2023-11-29 16:44:49 +0100

    shopping cart logic working

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import Button from "@/app/components/Button";
+import ProductImage from "@/app/components/products/ProductImage";
 import SetColor from "@/app/components/products/SetColor";
 import SetQuantity from "@/app/components/products/SetQuantity";
+import { useCart } from "@/hooks/useCart";
 import { Rating } from "@mui/material";
-import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { MdCheckCircle } from "react-icons/md";
 
 interface ProductDetailsProps {
 	product: any;
@@ -32,6 +36,8 @@ const Horizontal = () => {
 };
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
+	const { handleAddProductToCart, cartProducts } = useCart();
+	const [isProductInCart, setIsProductInCart] = useState(false);
 	const [cartProduct, setCartProduct] = useState<CartProductType>({
 		id: product.id,
 		name: product.name,
@@ -42,6 +48,22 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 		quantity: 1,
 		price: product.price,
 	});
+
+	const router = useRouter();
+
+	// console.log(cartTotalQty)
+
+	useEffect(() => {
+		setIsProductInCart(false);
+		if (cartProducts) {
+			const existingIndex = cartProducts.findIndex(
+				(item) => item.id === product.id
+			);
+			if (existingIndex > -1) {
+				setIsProductInCart(true);
+			}
+		}
+	}, [cartProducts]);
 
 	const productRating =
 		product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
@@ -83,7 +105,11 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-			<div>Images</div>
+			<ProductImage
+				cartProduct={cartProduct}
+				product={product}
+				handleColorSelect={handleColorSelect}
+			/>
 			<div className="flex flex-col gap-1 text-slate-500 text-sm">
 				<h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
 				<div className="flex items-center gap-2">
@@ -103,21 +129,40 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 					{product.inStock ? "In stock" : "Out of stock"}
 				</div>
 				<Horizontal />
-				<SetColor
-					cartProduct={cartProduct}
-					images={product.images}
-					handleColorSelect={handleColorSelect}
-				/>
-				<Horizontal />
-				<SetQuantity
-					cartProduct={cartProduct}
-					handleQtyIncrease={handleQtyIncrease}
-					handleQtyDecrease={handleQtyDecrease}
-				/>
-				<Horizontal />
-				<div className="max-w-[300px]">
-					<Button label="Add To Cart" onClick={() => {}} />
-				</div>
+				{isProductInCart ? (
+					<>
+					<p className="mb-2 text-slate-500 flex items-center gap-1">
+						<MdCheckCircle size={20} className="text-teal-400"/>
+						<span>Product added to cart</span>
+					</p>
+					<div className="max-w-[300px]">
+						<Button label='View Cart' outline onClick={()=>{
+							router.push('/cart')
+						}}/>
+					</div>
+					</>
+				) : (
+					<>
+						<SetColor
+							cartProduct={cartProduct}
+							images={product.images}
+							handleColorSelect={handleColorSelect}
+						/>
+						<Horizontal />
+						<SetQuantity
+							cartProduct={cartProduct}
+							handleQtyIncrease={handleQtyIncrease}
+							handleQtyDecrease={handleQtyDecrease}
+						/>
+						<Horizontal />
+						<div className="max-w-[300px]">
+							<Button
+								label="Add To Cart"
+								onClick={() => handleAddProductToCart(cartProduct)}
+							/>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,16 +1,22 @@
 import Container from "@/app/components/Container";
-import { product } from "@/utils/product";
 import ProductDetails from "./ProductDetails";
+import ListRating from "./ListRating";
+import { products } from "@/utils/products";
 
 interface IParams {
 	productId?: string;
 }
 
 const Product = ({ params }: { params: IParams }) => {
+	const product = products.find((item) => item.id === params.productId);
 	return (
 		<div className="p-8">
 			<Container>
 				<ProductDetails product={product} />
+				<div className="flex-col flex mt-20 gap-4">
+					<div>Add Rating</div>
+					<ListRating product={product} />
+				</div>
 			</Container>
 		</div>
 	);
